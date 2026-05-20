cb1cf88a9c40d37b952373f1d77b4421f7447775
zhaoyu217 <takepea1123@gmail.com>
2026-05-20 18:44:52 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const CACHE_NAME = 'hivedash-v2';
-const ASSETS = ['/app.html', '/index.html'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Always go to network first, fall back to cache
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+    .then(() => self.registration.unregister())
   );
 });

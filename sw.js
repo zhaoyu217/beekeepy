8c0207cc21e9792aecac0fc0eba9c9955b7a9bf5
zhaoyu217 <takepea1123@gmail.com>
2026-05-20 18:51:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
     .then(() => self.clients.claim())
-    .then(() => self.registration.unregister())
   );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
